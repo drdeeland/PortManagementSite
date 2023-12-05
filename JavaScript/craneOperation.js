a1a271368b50cd43f3ca354d039f16aaafd782e0
christieFo <144366950+christieFo@users.noreply.github.com>
2023-12-05 18:56:27 -0500

    Add files via upload

---
+++ JavaScript/craneOperation.js
@@ -0,0 +1,127 @@
+const urlBase = CONSTANT.urlBase;
+const extension = CONSTANT.extension;
+
+function setContainerStatus(){
+    let id = document.getElementById("sourceid").value;
+    let isShip;
+  
+    if (document.getElementById('destination1').checked) {
+      isShip = true;
+    } else if (document.getElementById('destination2').checked) {
+      isShip = false;
+    }
+  
+    // let containerList = "";
+    // let containerTable = "<tr><th>Container ID</th><th>Company</th><th>Source ID</th><th>Storage ID</th><th>Destination ID</th><th>Status</th></tr>";
+  
+      tmp = {sourceId:id, isShip};
+    let jsonPayload = JSON.stringify( tmp );
+    
+    let url = urlBase + '/updateContainerStatus' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    let result;
+
+    try {
+        xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = xhr.responseText;
+            console.log(response);
+
+            document.getElementById("updateContainerStatus").innerHTML = "Containers successfully updated";
+        }
+    };
+
+    xhr.send(jsonPayload);
+    }
+    catch(err) {
+    document.getElementById("updateStatus").innerHTML = err.message;
+    }
+
+    return result;
+}
+
+function getSourceContainers() {
+    // console.log("retrieve func");
+
+    let id = document.getElementById("sourceid");
+    let isShip;
+  
+    if (document.getElementById('destination1').checked) {
+      isShip = true;
+    } else if (document.getElementById('destination2').checked) {
+      isShip = false;
+    }
+  
+    let containerList = "";
+    let containerTable = "<tr><th>Container ID</th><th>Company</th><th>Source ID</th><th>Storage ID</th><th>Destination ID</th><th>Status</th></tr>";
+  
+      tmp = {sourceId:id, isShip};
+    let jsonPayload = JSON.stringify( tmp );
+    
+    let url = urlBase + '/getSourceContainers' + extension;
+  
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  
+    let result;
+  
+    try 
+    {
+      xhr.onreadystatechange = function() 
+      {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+          let response = xhr.responseText;
+          console.log(response);
+          let jsonObject = JSON.parse( response );
+  
+          if (jsonObject.error != null) {
+            console.log(jsonObject.error);
+          }
+  
+          for (let i = 0; i < jsonObject.results.length; i++) 
+          {
+            containerList += jsonObject.results[i];
+  
+            containerTable += "<tr><td>Container: " + jsonObject.results[i]["container_id"] + "</td>"
+
+            containerTable += "<td>Company: " + jsonObject.results[i]["company"] + "</td>"
+            
+            if (jsonObject.results[i]["source_sid"] == "") {
+              containerTable += "<td>Truck: " + jsonObject.results[i]["source_tid"] + "</td>"
+            }
+            else {
+              containerTable += "<td>Ship: " + jsonObject.results[i]["source_sid"] + "</td>"
+            }
+  
+            containerTable += "<td>Storage: " + jsonObject.results[i]["storage_id"] + "</td>"
+            
+            if (jsonObject.results[i]["dest_sid"] == "") {
+              containerTable += "<td>Truck: " + jsonObject.results[i]["dest_tid"] + "</td>"
+            }
+            else {
+              containerTable += "<td>Ship: " + jsonObject.results[i]["dest_sid"] + "</td>"
+            }
+  
+            containerTable += "<td>" + jsonObject.results[i]["location"] + "</td></tr>"
+          }
+  
+          document.getElementById("containers").innerHTML = containerTable;
+        }
+      };
+      xhr.send(jsonPayload);
+    }
+    catch (err) 
+    {
+      console.log(err);
+      return null;
+    }
+  
+    return result;
+  }
+  
---
+++ JavaScript/portExit.js
@@ -0,0 +1,46 @@
+const urlBase = CONSTANT.urlBase;
+const extension = CONSTANT.extension;
+
+function setShipDeparture(){
+    console.log("ship departs");
+    // extra Credit:
+        // Implement a queuing system. If berths are full and ships attempt to check in, the system should 
+        // add them to a queue. This queue should automatically update when a berth becomes available. 
+
+    let shipId = document.getElementById("sid");
+    // if (queue exists){
+    //     let queuedship = "";
+    // }
+
+    let tmp = {"ship_id":id};
+    let jsonPayload = JSON.stringify( tmp );
+
+    let url = urlBase + '/leaveBerth' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    let result;
+
+    try 
+    {
+    xhr.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+        let response = xhr.responseText;
+        console.log(response);
+        }
+    };
+    xhr.send(jsonPayload);
+    }
+        catch (err) 
+    {
+    console.log(err);
+        return null;
+    }
+    document.getElementById("leaveBerthMessage").innerHTML = "Ship successfully left Berth.";
+
+    return result;
+}
